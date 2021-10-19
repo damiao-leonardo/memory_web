d82d782d5a93d26353c8b340f21e24287d7e2bf9
leonardo <leonardo181212@gmail.com>
2021-10-18 21:11:05 -0300

    modify file env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const token = localStorage.getItem('@memory:token');
 
+console.log(process.env.REACT_APP_API_URL);
+
 const api = axios.create({
  baseURL: process.env.REACT_APP_API_URL,
  headers: { Authorization: `Bearer ${token}` },
